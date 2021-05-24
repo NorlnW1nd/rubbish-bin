2fe4e02eea6d6f847b4d91af7b0a489cc4b308a5
NorlnW1nd <792847726@qq.com>
2021-05-24 23:18:10 +0800

    创建项目

---
+++ src/main/java/com/fer/RubbishBinApplication.java
@@ -0,0 +1,13 @@
+package com.fer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubbishBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubbishBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fer/RubbishBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubbishBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
